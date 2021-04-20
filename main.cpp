afe49008c068cc9bd68d321af842a765055ad7e0
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2021-04-20 21:55:03 +0300

    Added Libraries {cstring, chrono}

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstring>
+#include <chrono>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
