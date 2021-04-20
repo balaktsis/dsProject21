bffe6f719b3a54151a22b6d09616c3b4de4cf4c8
Neron <ripstikerpro@gmail.com>
2021-04-20 21:48:20 +0300

    added gitignore

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <chrono>
-#include <cstring>
-
-using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
