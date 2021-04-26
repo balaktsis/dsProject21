84236879bdb4cf5720c4eddb4c8a1478e72020ce
Neron <ripstikerpro@gmail.com>
2021-04-26 18:17:05 +0300

    Finished orderedArray - kai kala

--- orderedArray.cpp
+++ orderedArray.cpp
@@ -3,3 +3,116 @@
 //
 
 #include "orderedArray.h"
+#include <string>
+
+using namespace std;
+orderedArray::orderedArray() {
+    data = nullptr;
+    num = nullptr;
+    size = 0;
+}
+
+void orderedArray::copyFromUnordered(string *newData, int *newNum, long arraySize) {
+    this->data = newData;
+    this->num = newNum;
+    size = arraySize;
+
+    //TODO:Quicksort
+
+}
+
+bool orderedArray::binSearch(const string &word, long &pos) {
+    long begin = 0, end = size-1;
+    long mid = (begin+end)/2;
+
+    while (mid != end || mid != begin){
+        if (word > data[mid]){
+            begin = mid;
+        } else {
+            end = mid;
+        }
+        mid = (begin+end)/2;
+    }
+
+    pos = mid;
+    return data[mid] == word;
+
+}
+
+void orderedArray::insert(const string& word) {
+    long pos;
+    if (binSearch(word, pos)){
+        num[pos] += 1;
+        return;
+    }
+
+    string *newData = new string[size+1];
+    int *newNum = new int[size+1];
+    copy(&data[0],&data[pos] , newData);
+    copy(&num[0],&num[pos] , newNum);
+    newData[pos+1] = word;
+    newNum[pos+1] = 1;
+    for (int i = pos+1; i < size; ++i) {
+        newData[i+1] = data[i];
+        newNum[i+1] = num[i];
+    }
+    size++;
+    data = newData;
+    num = newNum;
+    delete[] newNum;
+    delete[] newData;
+
+}
+
+void orderedArray::insertUnique(const string word, const int occurrences) {
+    long pos;
+    if (binSearch(word, pos)){  //If the word already exists, discard the operation
+        return;
+    }
+
+    string *newData = new string[size+1];
+    int *newNum = new int[size+1];
+    copy(&data[0],&data[pos] , newData);
+    copy(&num[0],&num[pos] , newNum);
+    newData[pos+1] = word;
+    newNum[pos+1] = occurrences;
+    for (int i = pos+1; i < size; ++i) {
+        newData[i+1] = data[i];
+        newNum[i+1] = num[i];
+    }
+    size++;
+    data = newData;
+    num = newNum;
+    delete[] newNum;
+    delete[] newData;
+}
+
+int orderedArray::search(const string word) {
+    long pos;
+    return binSearch(word, pos) ? num[pos] : 0;
+}
+
+void orderedArray::remove(const string word) {
+    long pos;
+    if (!binSearch(word, pos)){  //If the word already exists, discard the operation
+        return;
+    }
+
+    string *newData = new string[size-1];
+    int *newNum = new int[size-1];
+    copy(&data[0],&data[pos-1] , newData);
+    copy(&num[0],&num[pos-1] , newNum);
+    for (int i = pos; i < size; ++i) {
+        newData[i] = data[i+1];
+        newNum[i] = num[i+1];
+    }
+    size--;
+    data = newData;
+    num = newNum;
+    delete[] newNum;
+    delete[] newData;
+}
+
+int orderedArray::getSize() {
+    return size;
+}
--- orderedArray.h
+++ orderedArray.h
@@ -9,16 +9,17 @@
 using namespace std;
 class orderedArray {
 private:
-    string *data;               //Array storing the words (kept in relation to num)
+    string *data;               //Array storing the words (kept in relation to num).
     int *num;                   //Array storing the occurrences of words.
-    long size;                  //Size of the array
+    long size;                  //Size of the array.
+    bool binSearch(const string &word, long &pos);   //Binary search to be used internally.
 public:
     orderedArray();
-    void copyFromUnordered(string *newData, int *newNum);           //Copy a non sorted array and sort it.
-    void insert(const string word);                                 //Get a word and insert it in the correct order/increment it's appearance number
-    void insertUnique(const string word, const int occurrences);    //Get a word and occurrence number and place it in the correct spot
-    int search(const string word);                                  //Search for a word and return it's occurrences (0 for none)
-    bool remove(const string word);                                 //Remove word from the array and readjust the order
+    void copyFromUnordered(string *newData, int *newNum, long arraySize);           //Copy a non sorted array and sort it.
+    void insert(const string& word);                                //Get a word and insert it in the correct order/increment it's appearance number.
+    void insertUnique(string word, int occurrences);    //Get a word and occurrence number and place it in the correct spot.
+    int search(string word);                                  //Search for a word and return it's occurrences (0 for none).
+    void remove(string word);                                 //Remove word from the array and readjust the order.
     int getSize();                                                  //Returns the size of the array.
 
 
