52c40358f4d6fbca732444e8d0e0e08c6c1ae388
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2021-05-11 19:38:05 +0300

    Creation of AVLtree class.

---
+++ AVLtree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christos Balaktsis on 11/5/2021.
+//
+
+#include "AVLtree.h"
---
+++ AVLtree.h
@@ -0,0 +1,14 @@
+//
+// Created by Christos Balaktsis on 11/5/2021.
+//
+
+#ifndef TEST_AVLTREE_H
+#define TEST_AVLTREE_H
+
+
+class AVLtree {
+
+};
+
+
+#endif //TEST_AVLTREE_H
